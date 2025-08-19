c6a603fe0db9150ddf2628fbe21b04025ff7518f
Shahboz Nuriddinov <139639356+Shahboz-dev@users.noreply.github.com>
2025-08-19 10:23:55 +0500

    Add files via upload

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+// env.d.ts
+declare namespace NodeJS {
+  interface ProcessEnv {
+    OPENAI_API_KEY?: "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";
+    SERPER_API_KEY?: "91bac19cdfaafbc909e0b8129e03b92919820c48";
+  }
+}
